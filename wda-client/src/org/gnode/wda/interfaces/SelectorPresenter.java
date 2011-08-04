31194fcd0e5cb704ba48a82d6363ce35650b2f1f
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2011-08-05 00:59:27 +0530

    Deleting stray files from previous commit

--- wda-client/src/org/gnode/wda/interfaces/SelectorPresenter.java
+++
@@ -1,9 +0,0 @@
-package org.gnode.wda.interfaces;
-
-public interface SelectorPresenter {
-	public void setNodesDisplay(SelectorNodesDisplay snd);
-	public SelectorNodesDisplay getNodesDisplay();
-	
-	public void setTreeDisplay(SelectorTreeDisplay std);
-	public SelectorTreeDisplay getTreeDisplay();
-}
